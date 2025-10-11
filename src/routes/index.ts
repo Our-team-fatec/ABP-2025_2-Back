1c6a5c68f28f57115c7ca410a56ae7cf921f26ba
lucasdwn <lfcosta0804@gmail.com>
2025-10-10 21:54:52 -0300

    feat: crud anuncios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import userRoutes from "./usersRoutes";
 import healthRoutes from "./healthRoutes";
 import authRoutes from "./authRoutes";
 import petsRoutes from "./petsRoutes";
+import adocoesRoutes from "./adocoesRoutes";
 
 const routes = Router();
 routes.use("/auth", authRoutes);
 routes.use("/users", userRoutes);
 routes.use("/pets", petsRoutes);
+routes.use("/adocoes", adocoesRoutes);
 routes.use("/health", healthRoutes);
 export default routes;
